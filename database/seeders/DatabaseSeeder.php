9a2109882371156fecf9a3eb226c4d5c91c9b29b
hoangsonhv <70937660+hoangsonhv@users.noreply.github.com>
2021-07-02 00:27:50 +0700

    admin update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table("admins")->insert([
+            "name"=>"Administrator",
+            "email"=>"admin1@gmail.com",
+            "password"=>bcrypt("12345678")
+        ]);
     }
 }
